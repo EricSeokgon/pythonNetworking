e4b32b66c425dcced8ec75dc9702009f3db651d3
hadesyi <leesk55@gmail.com>
2022-12-09 15:27:03 +0900

    입장료 계산

---
+++ chapter3/price.py
@@ -0,0 +1,11 @@
+# 입장료 계산
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+elif age < 65:
+    price = 10
+else:
+    price = 5
+print("Your admission cost is $" + str(price) + ".")
